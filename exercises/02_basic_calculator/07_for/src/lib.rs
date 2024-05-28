ab18156dd85d084e3fb4144d61cba174def10216
felix diaz <felixmdm321@gmail.com>
2024-05-28 00:01:40 -0700

    exercises 0-7 completed, thumbs up emoji

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,13 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+
+    let mut result = 1;
+
+    for mut i in 1..n {
+        result *= i;
+        i += 1;
+    }
+    result
 }
 
 #[cfg(test)]
